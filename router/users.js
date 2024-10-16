a02cf5b7187abf07a77e58ac154011cc24b1ab2e
diego18040 <dieg00.lopez00@hotmail.com>
2024-10-16 16:37:21 -0600

    Agregamos funcion de create

--- router/users.js
+++ router/users.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {allUser ,userByRole } from "../controllers/users/read.js";
+import { create } from "../controllers/users/create.js";
 
 const routerUser = Router()
 
 routerUser.get('/all', allUser)
 routerUser.get('/role/:x',userByRole)
+routerUser.post('/create',create)
 
 export default routerUser
